466ef6a3bfcfe6e74f1807f2482d3a3eaf0ae081
Luke <up858296@myport.ac.uk>
2020-02-25 14:07:31 +0000

    calcutaion for bmi and bmr now working

--- src/components/setup_page.js
+++ src/components/setup_page.js
@@ -2,20 +2,23 @@ import React from 'react';
 
 
     function calculate_bmi() {
-        let Weight = document.getElementsByClassName('bmi_weight').value;
-        let Height = document.getElementsByClassName('bmi_height').value;
-        let Age = document.getElementsByClassName('bmi_age').value;
-        let Test = document.getElementsByClassName('test_print');
-        let bmi_calc = Weight / Height / Height * 10000;
-        let bmr_calc = 88.2 + (13.362 + Weight) + (4.799 + Height) - (5.677 + Age);
-
+        let Weight = document.getElementById('bmi_weight').value;
+        let Height = document.getElementById('bmi_height').value;
+        let Age = document.getElementById('bmi_age').value;
+        let Test = document.getElementById('test_print');
+        let bmi_calc = Weight / Height^2 * 10000;
+        let bmr_calc = 88.2 + (13.362* Weight) + (4.799 * Height) - (5.677 * Age);
+  
+        console.log (
+            bmr_calc
+        );
         //if nothing entered show error
-        if (Weight, Height, Age == ""){
+        if (Weight == 0){
             alert("Please enter your Height, Weight and Age so we can achieve your goals!")
             }
             else{
             //
-            Test.innerHTML = document.write= bmi_calc + bmr_calc;
+            Test.innerHTML = document.write= bmi_calc + ' ' + bmr_calc;
             }
 
     }
@@ -23,11 +26,11 @@ import React from 'react';
 function setup_page () {
     return (
     <div className='setup_main'>
-     <input className= 'setup_input' type='number' placeholder='Weight in kg:'/>
-     <input className= 'setup_input' type='number' placeholder='Height in cm:'/>
-     <input className= 'setup_input' type='number' placeholder='Age:'/>
+     <input id= 'bmi_weight' className= 'setup_input' type='number' placeholder='Weight in kg:'/>
+     <input id= 'bmi_height' className= 'setup_input' type='number' placeholder='Height in cm:'/>
+     <input id= 'bmi_age' className= 'setup_input' type='number' placeholder='Age:'/>
      <button onClick={calculate_bmi} className='bmi_button'>calculate</button>
-     <p className='test_print'></p>
+     <p id='test_print'></p>
      </div>
     );
     }
