9ed9d0dc0f50f9e1240278ed5d3584fee2cb9ab7
Luke <up858296@myport.ac.uk>
2020-02-26 23:18:46 +0000

    pulled data from firebase now stored in individual vars

--- src/components/main_page.js
+++ src/components/main_page.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import { database } from './firebase'
 
+
+var bmi;
+var bmr;
+var goal;
+var health;
 function main_page() {
+
+    let print = document.getElementById('printer');
+
+
+    database.collection('Health_data')
+        .doc('User_data')
+        .get()
+        .then(doc => {
+            const data = doc.data();
+            bmi = data.BMI;
+            bmr = data.BMR;
+            goal = data.Goal;
+            health = data.BMI_Health;
+            console.log(data);
+            console.log(bmi, bmr, goal, health);
+        })
     return (
         <div>
-        <p id='test_print'></p>
+            <p id='printer'></p>
         </div>
     );
 }
