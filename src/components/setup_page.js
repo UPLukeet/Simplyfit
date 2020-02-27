513e75af7f738abd0be15f183a2328d56f031b39
Luke <up858296@myport.ac.uk>
2020-02-27 18:58:33 +0000

    added styling for setup page

--- src/components/setup_page.js
+++ src/components/setup_page.js
@@ -107,56 +107,113 @@ function Units_Switch() {
 
 function setup_page() {
     return (
-        <div className='setup_main'>
-            <select id="gender" className='setup_dropdown'>
-                <option value="" disabled selected hidden>Choose a gender:</option>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-            </select>
+        <div className='wrapper'>
+            <div className='setup_main'>
+
+                <p className='setup_text'>Input Gender:</p>
+                <select id="gender" className='setup_dropdown'>
+                    <option value="" disabled selected hidden>Choose a gender:</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                </select>
 
 
+                <p className='setup_text'>Input the goal you'd like to achieve:</p>
+                <select id="goal" className='setup_dropdown'>
+                    <option value="" disabled selected hidden>Choose a goal:</option>
+                    <option value="recomp">Recomp</option>
+                    <option value="lose">Lose fat</option>
+                    <option value="gain">Gain muscle</option>
+                </select>
 
-            <select id="goal" className='setup_dropdown'>
-                <option value="" disabled selected hidden>Choose a goal:</option>
-                <option value="recomp">Recomp</option>
-                <option value="lose">Lose fat</option>
-                <option value="gain">Gain muscle</option>
-            </select>
+                <p className='setup_text'>Input the units for Height/Weight:</p>
+                <select onClick={Units_Switch} id="units" className='setup_dropdown'>
+                    <option value="" disabled selected hidden>Choose Kg/cm or lbs/feet:</option>
+                    <option value="met">Metric(kg/cm)</option>
+                    <option value="imp">Imperial(lbs/feet)</option>
+                </select>
 
-            <select onClick={Units_Switch} id="units" className='setup_dropdown'>
-                <option value="" disabled selected hidden>Choose Kg/cm or lbs/feet:</option>
-                <option value="met">Metric(kg/cm)</option>
-                <option value="imp">Imperial(lbs/feet)</option>
-            </select>
+                <p className='setup_text'>Input Weight:</p>
+                <input id='bmi_weight' className='setup_input' type='number' placeholder='Weight:' />
 
-            <input id='bmi_weight' className='setup_input' type='number' placeholder='Weight:' />
+                <p className='setup_text'>Input Height:</p>
+                <select id="height_lbs" className='setup_dropdown'>
+                    <option value="" disabled selected hidden>Height:</option>
+                    <option value="152.4">5'0</option>
+                    <option value="154.95">5'1</option>
+                    <option value="157.48">5'2</option>
+                    <option value="160.02">5'3</option>
+                    <option value="162.56">5'4</option>
+                    <option value="165.1">5'5</option>
+                    <option value="167.74">5'6</option>
+                    <option value="170.18">5'7</option>
+                    <option value="172.72">5'8</option>
+                    <option value="175.26">5'9</option>
+                    <option value="177.8">5'10</option>
+                    <option value="180.34">5'11</option>
+                    <option value="182.88">6'0</option>
+                    <option value="185.45">6'1</option>
+                    <option value="187.96">6'2</option>
+                    <option value="190.5">6'3</option>
+                    <option value="193.04">6'4</option>
+                    <option value="195.58">6'5</option>
+                </select>
 
-            <select id="height_lbs" className='setup_dropdown'>
-                <option value="" disabled selected hidden>Height:</option>
-                <option value="152.4">5'0</option>
-                <option value="154.95">5'1</option>
-                <option value="157.48">5'2</option>
-                <option value="160.02">5'3</option>
-                <option value="162.56">5'4</option>
-                <option value="165.1">5'5</option>
-                <option value="167.74">5'6</option>
-                <option value="170.18">5'7</option>
-                <option value="172.72">5'8</option>
-                <option value="175.26">5'9</option>
-                <option value="177.8">5'10</option>
-                <option value="180.34">5'11</option>
-                <option value="182.88">6'0</option>
-                <option value="185.45">6'1</option>
-                <option value="187.96">6'2</option>
-                <option value="190.5">6'3</option>
-                <option value="193.04">6'4</option>
-                <option value="195.58">6'5</option>
-            </select>
+                <select id='bmi_height' className='setup_dropdown'>
+                    <option value="" disabled selected hidden>Height:</option>
+                    <option value="150">150</option>
+                    <option value="151">151</option>
+                    <option value="152">152</option>
+                    <option value="153">154</option>
+                    <option value="154">154</option>
+                    <option value="155">155</option>
+                    <option value="156">156</option>
+                    <option value="157">158</option>
+                    <option value="158">159</option>
+                    <option value="159">159</option>
+                    <option value="160">160</option>
+                    <option value="161">161</option>
+                    <option value="162">162</option>
+                    <option value="163">163</option>
+                    <option value="164">164</option>
+                    <option value="165">165</option>
+                    <option value="166">166</option>
+                    <option value="167">167</option>
+                    <option value="168">168</option>
+                    <option value="169">169</option>
+                    <option value="170">170</option>
+                    <option value="171">171</option>
+                    <option value="172">172</option>
+                    <option value="173">173</option>
+                    <option value="174">174</option>
+                    <option value="175">175</option>
+                    <option value="176">176</option>
+                    <option value="177">177</option>
+                    <option value="178">178</option>
+                    <option value="179">179</option>
+                    <option value="180">180</option>
+                    <option value="181">181</option>
+                    <option value="182">182</option>
+                    <option value="183">183</option>
+                    <option value="184">184</option>
+                    <option value="185">185</option>
+                    <option value="186">186</option>
+                    <option value="187">187</option>
+                    <option value="188">188</option>
+                    <option value="189">189</option>
+                    <option value="190">190</option>
+                    <option value="191">191</option>
+                    <option value="192">192</option>
+                    <option value="193">193</option>
+                    <option value="194">194</option>
+                    <option value="195">195</option>
+                </select>
 
-            <input id='bmi_height' className='setup_input' type='number' placeholder='Height:' />
-            <input id='bmi_age' className='setup_input' type='number' placeholder='Age:' />
-            <button onClick={calculate_bmi} className='bmi_button'>calculate</button>
-            <p id='test_print'></p>
+                <p className='setup_text'>Input Age:</p>
+                <input id='bmi_age' className='setup_input' type='number' placeholder='Age:' />
+                <button onClick={calculate_bmi} className='bmi_button'>calculate</button>
+                <p id='test_print'></p>
+            </div>
         </div>
     );
 }
