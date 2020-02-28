0ed3b9c9af37251b9e351ef0f3f9adfc6ef8e292
Luke <up858296@myport.ac.uk>
2020-02-28 20:12:16 +0000

    added calorie breakdown to the main page, function triggered on button press need to add onload

--- src/components/main_page.js
+++ src/components/main_page.js
@@ -6,9 +6,23 @@ var bmi;
 var bmr;
 var goal;
 var health;
-function main_page() {
 
-    let print = document.getElementById('printer');
+function pullData() {
+
+    //divides calories for meals
+    let meal_One;
+    let meal_Two;
+    let meal_Three;
+    let snack;
+
+    //gets html elements
+    let Meal_one_print = document.getElementById('meal_one_print');
+    let Meal_two_print = document.getElementById('meal_two_print');
+    let Meal_three_print = document.getElementById('meal_three_print');
+    let Snack_print = document.getElementById('snack_print');
+    let Goal_print = document.getElementById('goal_print');
+    let Bmi_print = document.getElementById('bmi_print');
+    let Health_print = document.getElementById('health_print');
 
 
     database.collection('Health_data')
@@ -20,12 +34,52 @@ function main_page() {
             bmr = data.BMR;
             goal = data.Goal;
             health = data.BMI_Health;
-            console.log(data);
-            console.log(bmi, bmr, goal, health);
         })
+        .then(function () {
+            meal_One = bmr * 0.3;
+            meal_Two = bmr * 0.3;
+            meal_Three = bmr * 0.3;
+            snack = bmr * 0.1;
+            Meal_one_print.innerHTML = document.write = meal_One;
+            Meal_two_print.innerHTML = document.write = meal_Two;
+            Meal_three_print.innerHTML = document.write = meal_Three;
+            Snack_print.innerHTML = document.write = snack;
+            Goal_print.innerHTML = document.write = goal;
+            Bmi_print.innerHTML = document.write = bmi;
+            Health_print.innerHTML = document.write = health;
+            console.log("Successfully pulled");
+        })
+
+    console.log(meal_One, meal_Two, meal_Three, snack)
+
+};
+
+function main_page() {
     return (
-        <div>
-            <p id='printer'></p>
+        <div className='main_Main'>
+            <div className='meal_divs'>
+                <p className='food_heading'>Status:</p>
+                <p className='food_text' id='goal_print'></p>
+                <p className='food_text' id='bmi_print'></p>
+                <p className='food_text' id='health_print'></p>
+            </div>
+            <div className='meal_divs'>
+                <p className='food_heading'>Meal one:</p>
+                <p className='food_text' id='meal_one_print'></p>
+            </div>
+            <div className='meal_divs'>
+                <p className='food_heading'>Meal two:</p>
+                <p className='food_text' id='meal_two_print'></p>
+            </div>
+            <div className='meal_divs'>
+                <p className='food_heading'>Meal three:</p>
+                <p className='food_text' id='meal_three_print'></p>
+            </div>
+            <div className='meal_divs'>
+                <p className='food_heading'>Snack:</p>
+                <p className='food_text' id='snack_print'></p>
+            </div>
+            <button onClick={pullData}>calculate</button>
         </div>
     );
 }
