914a567a49524ecda46f380a76f7785485a6a8fd
Luke-Desktop <up858296@myport.ac.uk>
2020-03-31 13:21:53 +0100

    added styling for settings page

--- src/components/settings_page.js
+++ src/components/settings_page.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Authentication } from './firebase'
-import { Link } from 'react-router-dom';
-
+import Button from '@material-ui/core/Button';
 
 function settings_page(props) {
 
@@ -15,11 +14,11 @@ function settings_page(props) {
 
 
         return (
-            <div>
+            <div className='main_settings'>
                 <div className='App_margin'/>
-                <p>Log out by pressing the button bellow</p>
+                <p>Log out by pressing the button below:</p>
                 
-                <button onClick={logout}>Log out</button>
+                <Button color="primary" variant="contained" onClick={logout}>Log out</Button>
             </div>
         );
 
